c71d97dd3efb19ac78734b262f2d137b3faa988d
Denis <omelched@gmail.com>
2021-03-13 14:05:03 +0300

    request fix

--- clock.py
+++ clock.py
@@ -6,7 +6,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    requests.get('https://0.0.0.0:5000/notify/')
+    requests.get('http://0.0.0.0:5000/notify/')
 
 
 sched.start()
