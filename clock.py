a26432419ed29323ecf00be1d74f2cb54933645f
Denis <omelched@gmail.com>
2021-03-13 14:51:06 +0300

    real data test

--- clock.py
+++ clock.py
@@ -8,7 +8,12 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    requests.get('https://{}.herokuapp.com/notify/'.format(server.config['HEROKU_APP_NAME']))
+    r = requests.get('https://ps5status.ru/api/data')
+    payload = [{r.json()['data']['shops'][shop]['name']: r.json()['data']['shops'][shop]['normal_link']} for shop in
+               r.json()['data']['shops'].keys() if r.json()['data']['shops'][shop]['normal_info']['available'] is True]
+
+    if payload:
+        requests.get('https://{}.herokuapp.com/notify/'.format(server.config['HEROKU_APP_NAME']))
 
 
 sched.start()
