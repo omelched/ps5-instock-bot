67925e3c9f39fb35998f8f04428a818f8cf2b233
Denis <omelched@gmail.com>
2021-03-13 14:09:32 +0300

    request fix

--- clock.py
+++ clock.py
@@ -1,12 +1,14 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 
+from app import server
+
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    requests.get('http://0.0.0.0:5000/notify/')
+    requests.get('https://{}.herokuapp.com/notify/'.format(server.config['HEROKU_APP_NAME']))
 
 
 sched.start()
