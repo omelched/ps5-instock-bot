4063b97ecb9897b97580d782000d2785a0acbc9f
Denis <omelched@gmail.com>
2021-03-13 12:39:12 +0300

    heroku app name default

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,7 @@ from utils import config
 server = Flask(__name__)
 server.config.from_object(config)
 
+try:
+    _ = server.config['HEROKU_APP_NAME']
+except KeyError
+    server.config['HEROKU_APP_NAME'] = 'ps5-instock-bot'
